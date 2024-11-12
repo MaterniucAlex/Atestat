c900fcf2ca79bd9ad2e92c4d88d5ea8edf7390e3
Alex <MaterniucAlex@gmail.com>
2024-11-12 09:34:48 +0200

    Added SDL_Image

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-libs  := -lSDL3 -lm
+libs  := -lSDL3 -lm -SDL3_image
 files := src/main.c  
 
 all:
--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include "SDL3/SDL.h"
+#include "SDL3_image/SDL_image.h"
 
 int main()
 {
-
   SDL_Init(SDL_INIT_VIDEO);
-  SDL_Window *window = SDL_CreateWindow("Atestat", 1270, 720, 0);
+  IMG_Init(IMG_INIT_PNG);
+  SDL_Window *window = SDL_CreateWindow("Algoritmi de sortare - Vizualizator", 1270, 720, 0);
   SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
 
   SDL_Event event;
@@ -26,6 +27,7 @@ int main()
 
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
+  IMG_Quit();
   SDL_Quit();
   return 0;
 }
